96005756b83bdf6078090a3c2df8cb091822f0b2
Pablo Sánchez <pablosanchezflores@gmail.com>
2021-03-20 01:20:12 +0100

    Added OMDB Service

---
+++ src/app/services/omdb.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+class OmdbService {
+  url = 'http://www.omdbapi.com/';
+  apiKey = '46741dd1';
+  type = 'movie';
+  /**
+   * Constructor of the Service with Dependency Injection
+   * @param http The standard Angular HttpClient to make requests
+   */
+  constructor(private http: HttpClient) { }
+
+  /**
+  * Get data from the OmdbApi
+  * map the result to return only the results that we need
+  *
+  * @param {string} title Search Term
+  * @returns Observable with the search results
+  */
+  searchData(title: string): Observable<any> {
+    return this.http.get(`${this.url}?s=${encodeURI(title)}&type=${this.type}&apikey=${this.apiKey}`).pipe(
+      map(results => results['Search'])
+    );
+  }
+
+  /**
+  * Get the detailed information for an ID using the "i" parameter
+  *
+  * @param {string} id imdbID to retrieve information
+  * @returns Observable with detailed information
+  */
+  getDetails(id: string) {
+    return this.http.get(`${this.url}?i=${id}&plot=full&apikey=${this.apiKey}`);
+  }
+}
+
+export default OmdbService;
