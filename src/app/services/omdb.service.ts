8a870b2bb1525a0d404476387014cc531c9c969a
Pablo Sánchez <pablosanchezflores@gmail.com>
2021-03-24 18:24:47 +0100

    Update interfaces name

--- src/app/services/omdb.service.ts
+++ src/app/services/omdb.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { IOmdbResponse } from './../interface/i-omdb-response';
+import { IOmdbResponse } from '../interfaces/i-omdb-response';
 
 @Injectable({
   providedIn: 'root'
