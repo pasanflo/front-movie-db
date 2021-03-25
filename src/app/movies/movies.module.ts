6b093ec1e2bd62bcddd1c8c9b36188d455b28eb2
Pablo Sánchez <pablosanchezflores@gmail.com>
2021-03-25 19:26:27 +0100

    added movie-details-component to module

--- src/app/movies/movies.module.ts
+++ src/app/movies/movies.module.ts
@@ -4,14 +4,15 @@ import { CommonModule } from '@angular/common';
 import { MoviesRoutingModule } from './movies-routing.module';
 import { MoviesComponent } from './movies.component';
 import { MovieService } from '../services/movie.service';
-
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
 
 @NgModule({
   declarations: [
     MoviesComponent,
+    MovieDetailsComponent
   ],
   providers: [
-    MovieService
+    MovieService,
   ],
   imports: [
     CommonModule,
