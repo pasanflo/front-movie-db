348081714b01de1dc496254dc7469684ced2fb49
Pablo Sánchez <pablosanchezflores@gmail.com>
2021-03-25 19:25:34 +0100

    updated services

--- src/app/movies/movies-routing.module.ts
+++ src/app/movies/movies-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
 import { MoviesComponent } from './movies.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'movies', pathMatch: 'full' },
-  { path: 'movies', component: MoviesComponent }
+  { path: 'movies', component: MoviesComponent },
+  { path: 'movie-details/:id', component: MovieDetailsComponent }
 ];
 
 
